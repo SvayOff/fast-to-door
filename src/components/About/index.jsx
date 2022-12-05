d4cf6d7f73b3d9d4da55bcf0b3a41813285f1d7a
SvayOff <marsiul2001@gmail.com>
2022-12-05 22:14:14 +0200

    added code for adaprive About component

--- src/components/About/index.jsx
+++ src/components/About/index.jsx
@@ -61,7 +61,7 @@ const About = () => {
                 beginning, they grew pineapples
                 <img
                   className="about__content-image"
-                  src="./images/about/about_1.png"
+                  src="./images/about/about_2.png"
                   alt="about"
                 />
                 , but later began to develop and grow other exotic fruits and vegetables.
@@ -137,6 +137,87 @@ const About = () => {
                 city. Our stores are located in all large cities of the CIS countries, and we all
                 grow on our plantations in Brazil, Thailand and Africa.
               </p>
+            </div>
+          </div>
+          <div className="about__adaptive">
+            <div className="about__adaptive-years">
+              <span className="active">1990</span>
+              <span>1992</span>
+              <span>1994</span>
+              <span>2001</span>
+              <span>2014</span>
+              <span>2015</span>
+              <span>2021</span>
+              <span>1990 - 2021</span>
+            </div>
+            <div className="about__adaptive-descriptions">
+              <div className="about__adaptive-inner">
+                <p className="about__adaptive-desc">
+                  It all started in 1990, then my father had an idea to create something that he and
+                  the rest lacked, since in those days very few people could get some quality goods,
+                  he decided to take everything into his own hands and started act.
+                </p>
+
+                <p className="about__adaptive-desc">
+                  The first step was building our own team. His team were his friends
+                  <img
+                    className="about__adaptive-image"
+                    src="./images/about/about_1.png"
+                    alt="about"
+                  />
+                  . Together they caught fire on one thing - the delivery of fruits and vegetables
+                  from abroad. Since there were no such products in the CIS, or there were, but in
+                  order to get them you had to try.
+                </p>
+
+                <p className="about__adaptive-desc">
+                  In 1994, my father's team flew to Thailand and started looking for a suitable
+                  location. After half a year, they found her and bought land there. From the very
+                  beginning, they grew pineapples
+                  <img
+                    className="about__adaptive-image"
+                    src="./images/about/about_2.png"
+                    alt="about"
+                  />
+                  , but later began to develop and grow other exotic fruits and vegetables.
+                </p>
+
+                <p className="about__adaptive-desc">
+                  They later began growing fruits and vegetables in countries other than Thailand,
+                  such as Brazil and Africa.
+                </p>
+
+                <p className="about__adaptive-desc">
+                  My father bought the company's shares from his team and transferred the business
+                  to me.
+                </p>
+
+                <p className="about__adaptive-desc">
+                  Over the course of a year, my father helped me sort out the business, we improved
+                  some of its branches, such as: delivery, storage and growing of better products,
+                  so that we were confident that we were providing you with quality goods. And at
+                  the end, I assembled my team.
+                </p>
+
+                <p className="about__adaptive-desc">
+                  We are a team of 8 people
+                  <img
+                    className="about__adaptive-image"
+                    src="./images/about/about_3.png"
+                    alt="about"
+                  />
+                  who share a common goal. Our goal is the sale and fast delivery of exotic fruits
+                  and vegetables
+                  <img
+                    className="about__adaptive-image"
+                    src="./images/about/about_4.png"
+                    alt="about"
+                  />
+                  to all CIS countries. Since few people can get exotic fruits or vegetables in
+                  their city. Our stores are located in all large cities of the CIS countries, and
+                  we all grow on our plantations in Brazil, Thailand and Africa.
+                </p>
+              </div>
             </div>
           </div>
         </div>
