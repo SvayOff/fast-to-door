1badaae371feedaf1845bf58064c39eb250d2ddb
SvayOff <marsiul2001@gmail.com>
2023-02-07 16:36:24 +0200

    fix product (visual)

--- src/components/ProductSkeleton/index.jsx
+++ src/components/ProductSkeleton/index.jsx
@@ -2,17 +2,15 @@ import ContentLoader from 'react-content-loader';
 
 const ProductSkeleton = (props) => (
   <ContentLoader
-    className="products__items"
+    className="products__item"
     speed={2}
     width={287}
-    height={350}
-    viewBox="4 0 287 350"
+    height={351}
+    viewBox="0 0 287 351"
     backgroundColor="#f5f5f5"
     foregroundColor="#ededed"
     {...props}>
-    <rect x="398" y="418" rx="0" ry="0" width="272" height="23" />
-    <rect x="9" y="0" rx="0" ry="0" width="279" height="351" />
-    <circle cx="91" cy="76" r="41" />
+    <rect x="0" y="0" rx="0" ry="0" width="279" height="351" />
   </ContentLoader>
 );
 
